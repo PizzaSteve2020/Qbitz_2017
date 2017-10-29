e8f7ba262e46f53a247e8326778a72a6defc6ae2
SteveQbitz <olson.steven.p@gmail.com>
2017-10-28 21:24:52 -0500

    edited switch statement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -12,37 +12,78 @@ public class Robot {
     private DcMotor frontRight;
     private DcMotor backLeft;
     private DcMotor backRight;
+    private double wheelCircumference = 32;
     public Robot(HardwareMap hardwareMap) {
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
     }
 
-    public void Drive(MechDriveDirections direction, double centimeters) {
+    public void Drive(MechDriveDirections direction, int centimeters) {
+        double centimetersDividedbyWheelCircumference = centimeters/wheelCircumference;
         switch(direction) {
-            case MechDriveDirections.North:
+            case North:
                 //John insert code here
                 break;
-            case MechDriveDirections.NorthEast:
+            case NorthEast:
                 //Steve insert code here
+                //One encoder tick in setTargetPosition is equal to about 0.026 cm using our 4 inch diameter wheels
+                //Our wheel circumference is about 32 centimeters
+
+                frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+                frontLeft.getCurrentPosition();
+                backRight.getCurrentPosition();
+
+                frontLeft.setTargetPosition( (int) Math.round( centimetersDividedbyWheelCircumference*1220 ) );
+                backRight.setTargetPosition( (int) Math.round( centimetersDividedbyWheelCircumference*1220 ) );
+
+                frontLeft.setPower(0.5);
+                backRight.setPower(0.5);
+
+                frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
                 break;
-            case MechDriveDirections.East:
+            case East:
                 //Mark insert code here
                 break;
-            case MechDriveDirections.SouthEast:
+            case SouthEast:
                 //Caroline insert code here
                 break;
-            case MechDriveDirections.South:
-                //Jborn insert code here
+            case South:
+            //Jborn insert code here
                 break;
-            case MechDriveDirections.SouthWest:
+            case SouthWest:
                 //Steve insert code here
+                //Same code as NorthEast but the opposite
+                frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+                frontLeft.getCurrentPosition();
+                backRight.getCurrentPosition();
+
+                frontLeft.setTargetPosition( -(int) Math.round( centimetersDividedbyWheelCircumference*1220 ) );
+                backRight.setTargetPosition( -(int) Math.round( centimetersDividedbyWheelCircumference*1220 ) );
+
+                frontLeft.setPower(-0.5);
+                backRight.setPower(-0.5);
+
+                frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
                 break;
-            case MechDriveDirections.West:
+            case West:
                 //Mark insert code here
                 break;
-            case MechDriveDirections.NorthWest:
+            case NorthWest:
                 //Caroline insert code here
                 break;
         }
