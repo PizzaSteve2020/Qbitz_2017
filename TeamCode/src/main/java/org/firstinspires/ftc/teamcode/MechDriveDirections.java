ddfb1b63ea5bc212e63c1c401dd06bff4b802aae
grace <gracekos20@gmail.com>
2017-10-29 20:56:03 -0500

    Created vector and position variables in Robot class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechDriveDirections.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechDriveDirections.java
@@ -1,9 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-/**
- * Created by Steve on 10/24/2017.
- */
-
 public enum MechDriveDirections {
     North, NorthEast, East, SouthEast, South, SouthWest, West, NorthWest
 
