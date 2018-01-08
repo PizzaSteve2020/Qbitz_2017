c75d7e47b924c25faac41fb9959fa7474dc34e4f
SteveQbitz <olson.steven.p@gmail.com>
2018-01-07 19:55:55 -0600

    Implemented Vuforia

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vuforia.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vuforia.java
@@ -2,23 +2,25 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
 /**
  * Created by Steve on 12/28/2017.
  */
 @Autonomous(name="Vuforia", group ="Vuforia")
 public class Vuforia extends AutonomousExtendableClass {
-
-
+    VuforiaLocalizer vuforia;
 
     @Override
     public void runOpMode() {
-        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontLeft= hardwareMap.get(DcMotor.class, "frontLeft");
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
 
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
@@ -40,23 +42,50 @@ public class Vuforia extends AutonomousExtendableClass {
         gripper2 = hardwareMap.get(Servo.class, "gripper2");
         jewelDisplacer = hardwareMap.get(Servo.class, "jewelDisplacer");
 
-        colorSensor = hardwareMap.get(ColorSensor.class, "colorSensor");
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
 
-        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            parameters.vuforiaLicenseKey="ATy+Mon/////AAAAmdO7ijESrEYksmtbbrhsJ0h8pd0hCk5Vcp0EgfFUzSv7qvMD+pVBK+McXiTjFO/nc1jCMers6XjWglIGEGM+2nQ62R4An/KtVZYx8pcAJvoMNsgmr/IjFu1kZX+X2cWrplyV9eHUgaL+IcWzVzzKUG0vHzffzcLeMzEWs3mafEs9geCmYuyIjEzhiqLdQHL2gYjrMNSbA5K4iFjlU6Gxka0sR4Q2/YtR1RDVGPjo+4Dv+m7/4cthw32VEb8Mw0+xiRWcAKdDZO2Tf7CKFr5T7h5abyv8il+Dw5vN61kkqChalpel/ebyj2sQf3Ag09Z2b/PRVtOmwIYwp4s5k/TkWdbc95RvS10IM8bOVlLUaGim";
+        parameters.cameraDirection= VuforiaLocalizer.CameraDirection.BACK;
 
-        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+
+        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+        VuforiaTrackable relicTemplate = relicTrackables.get(0);
+        relicTemplate.setName("relicVuMarkTemplate");
 
         waitForStart();
 
-        encoderDrive(TURN_SPEED, -16,16,5);
+        relicTrackables.activate();
 
 
+
+        while(opModeIsActive()) {
+            RelicRecoveryVuMark vuMark =  RelicRecoveryVuMark.from(relicTemplate);
+
+            if(vuMark!=RelicRecoveryVuMark.UNKNOWN) {
+
+                telemetry.addData("VuMark", "%s visible", vuMark);
+                telemetry.update();
+
+                switch(vuMark) {
+                    case LEFT:
+                        encoderDrive(1,30,0,5);
+                        break;
+                    case CENTER:
+                        encoderDrive(1,30,30,5);
+                        break;
+                    case RIGHT:
+                        encoderDrive(1,0,30,5);
+                        break;
+
+
+                }
+
+                }
+
+
+
+        }
     }
-
 }
